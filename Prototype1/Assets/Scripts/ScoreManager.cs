34f1295d38ac1dbc291de3d1770ebc7c768beae9
PlatFormPlayZ <zjrw@icloud.com>
2022-09-04 15:01:44 -0500

    Added Prototype 1 Runthorough

---
+++ Prototype1/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    //notice public static variables can be accessed from any script
+    //but cannot be seen in the inspector
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    //set this in the inpsector
+    public Text textbox;
+
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        if(score>=3)
+        {
+            won = true;
+            gameOver = true;
+        }
+
+        if(gameOver)
+        {
+            if(won)
+            {
+                textbox.text = "You win!\nPress R to Try Again!";
+            }
+            else
+            {
+                textbox.text = "You lose!\nPress R to Try Again!";
+            }
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
